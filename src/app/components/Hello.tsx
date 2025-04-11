2875ae79c8caef0779fec1858c6dd31b90e3f751
Romeo <ralonsoad@gmail.com>
2025-04-11 14:48:40 +0200

    server and client  componets

---
+++ src/app/components/Hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+function Hello() {
+  console.log(" I am a client component")
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+  // <h1 className="text-3xl"> welcome to DACN. </h1>
+
+}
+export default Hello;
